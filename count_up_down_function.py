bbc9834e65d5a6c194b6a77fe999afd1956d0d66
mumarshakar <mumarshakar@gmail.com>
2024-05-14 11:19:42 +0400

    Recursion of function

--- count_up_down_function.py
+++ count_up_down_function.py
@@ -5,14 +5,14 @@
 # If number is negative then count upwards 
 
 x = int(input("Enter Number: "))  # Getting input from the user as integer
-def count(n):   # function declaration for count
+def count(n):   # function declaration for count with parameter n
     if n > 0:       # Condition if the number is greater than 0
         print(n)    # printing the value of n which entered by user
-        count(n-1)
-    elif n < 0:
-        print(n)
-        count(n+1)
-    else:
-        print("Blast Off")
+        count(n-1)  # Self Calling of function  OR Recursion 
+    elif n < 0:         # Second Part of condition  if number entered by user is less than zero
+        print(n)        # printing value of n
+        count(n+1)      # Self Calling of function or recursion 
+    else:               # if none of the above condition is true then else will be executed
+        print("Blast Off")  # printing value "Blast Off"
 
-count(x)
+count(x)        # Function Call with argument x 
