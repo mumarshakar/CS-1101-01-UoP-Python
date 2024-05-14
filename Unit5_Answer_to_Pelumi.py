ad7f2dfdabba831834dddacf37b078a79b835bed
mumarshakar <mumarshakar@gmail.com>
2024-05-14 16:12:16 +0400

    This is the answer to question of mate pelumi in discussion forum

---
+++ Unit5_Answer_to_Pelumi.py
@@ -0,0 +1,9 @@
+# This code is for pelumi's post answer of the question
+
+prefix = ["J", "K", "L", "M", "N", "Ou", "P", "Qu"]
+sufix = "ack"
+
+for i in prefix:
+    result = i + sufix
+    print(result)
+    
