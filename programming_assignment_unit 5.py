4a1f0395462e15b9e01dc8d39b04ee0db1a0a5fb
mumarshakar <mumarshakar@gmail.com>
2024-05-14 12:01:02 +0400

    Assignment Complete code before submission

--- programming_assignment_unit 5.py
+++ programming_assignment_unit 5.py
@@ -8,7 +8,44 @@ The code and its output must be explained technically.
  or in the form of comments within the code. The descriptive part of
  your response must be at least 200 words. 
 """
-name = input("Enter Your Name: ")
-def name(n):
-    print(n)
-name(name)
+
+def get_name():
+  """Prompts the user to enter their name and returns it."""
+  name = input("Enter your name: ")
+  return name
+# nm = get_name()
+# print(nm)
+
+
+def characters(nm, n):
+  """Displays the first n characters of the name."""
+  print(f"First {n} characters: {nm[:n]}")
+
+# characters("UMARSHAKAR", 3)
+
+def count_vowels(name):
+  """Counts the number of vowels in the name."""
+  vowels = 'aeiouAEIOU'
+  count = 0
+  for char in name:
+    if char in vowels:
+      count += 1
+  print(f"Number of vowels: {count}")
+
+# count_vowels("MUHAMMAD")
+
+def reverse_name(name):
+  """Reverses the name and displays it."""
+  print(f"Reversed name: {name[::-1]}")
+#reverse_name("PAKISTAN")
+
+
+
+def main():
+  """Gets the name from the user and performs the operations."""
+  name = get_name()
+  n = int(input("Enter the number of characters to display (from left): "))
+  characters(name, n)
+  count_vowels(name)
+  reverse_name(name)
+main()
